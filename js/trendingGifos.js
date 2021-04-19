ca9ec21ec888a1a152a941cfbde025cd9b648a85
jhonnyrave <jhonnyrave09@hotmail.com>
2021-04-18 21:36:12 -0500

    cambios

--- js/trendingGifos.js
+++ js/trendingGifos.js
@@ -3,30 +3,53 @@ const $flecha_izquierda = document.querySelector("#previous-btn");
 const $flecha_derecha = document.querySelector("#next-btn");
 const $gitMaximized = document.querySelector("#maximizedGif");
 const $maxCloseBtn = document.querySelector("#close-max-btn");
+const $carousel_modal = document.querySelector("#trending__slider-modal");
 const $flecha_izquierda_modal = document.querySelector("#previous-btn-modal");
 const $flecha_derecha_modal = document.querySelector("#next-btn-modal");
+const carrusel_img = document.getElementsByClassName("gif__container_modal");
+const $maxGif_Container = document.querySelector("#maxGif_Container");
 
 $flecha_izquierda.addEventListener("mouseout", () => {
   $flecha_izquierda.src = "assets/button-slider-left.svg";
+});
+
+$flecha_izquierda_modal.addEventListener("mouseout", () => {
+  $flecha_izquierda_modal.src = "assets/button-slider-left.svg";
 });
 
 $flecha_derecha.addEventListener("mouseout", () => {
   $flecha_derecha.src = "assets/Button-Slider-right.svg";
 });
 
+$flecha_derecha_modal.addEventListener("mouseout", () => {
+  $flecha_derecha_modal.src = "assets/Button-Slider-right.svg";
+});
+
 $flecha_izquierda.addEventListener("mouseover", () => {
   $flecha_izquierda.src = "assets/button-slider-left-hover.svg";
+});
+
+$flecha_izquierda_modal.addEventListener("mouseover", () => {
+  $flecha_izquierda_modal.src = "assets/button-slider-left-hover.svg";
 });
 
 $flecha_derecha.addEventListener("mouseover", () => {
   $flecha_derecha.src = "assets/Button-Slider-right-hover.svg";
 });
 
+$flecha_derecha_modal.addEventListener("mouseover", () => {
+  $flecha_derecha_modal.src = "assets/Button-Slider-right-hover.svg";
+});
+
 const apiTrendingGifs = async () => {
   await fetch(`${giphyAPI}?api_key=${apiKey}`)
     .then((response) => response.json())
     .then((trendingGif) => {
+      localStorage.clear();
+      let gifos = [];
+      gifos.push(trendingGif);
       console.log(trendingGif);
+      localStorage.setItem("GifosArray", JSON.stringify(gifos));
       carouselTrendingGifs(trendingGif);
     })
     .catch((err) => console.log(err));
@@ -39,7 +62,7 @@ const carouselTrendingGifs = (trendingGif) => {
     const div = document.createElement("div");
     div.classList.add("gif__container");
     div.innerHTML = ` 
-    <img class="gif" onclick="maximizeGif('${trendingGif.data[i].images.original.url}','${trendingGif.data[i].username}','${trendingGif.data[i].title}')" src="${trendingGif.data[i].images.original.url}" alt="${trendingGif.data[i].title}">
+    <img class="gif" onclick="maximizeGif('${trendingGif.data[i].images.original.url}')" src="${trendingGif.data[i].images.original.url}" alt="${trendingGif.data[i].title}">
 
     <div class="gifActions">
         <div class="btn-gitActions">
@@ -65,39 +88,74 @@ const prevSliderBtn = () => {
   $carousel.scrollLeft += 400;
 };
 
+// Flechas modal
+const nextSliderBtn_modal = () => {
+  console.log(carrusel_img);
+  for (let index = 0; index < carrusel_img.length; index++) {
+    if (carrusel_img[index].style.display == "flex") {
+      let conteo = index - 1;
+      if (conteo < 0) {
+        conteo = 49;
+      }
+      carrusel_img[index].style.display = "none";
+      carrusel_img[conteo].style.display = "flex";
+    }
+  }
+};
+
+const prevSliderBtn_modal = () => {
+  console.log(carrusel_img);
+  for (let index = 0; index < carrusel_img.length; index++) {
+    if (carrusel_img[index].style.display == "flex") {
+      carrusel_img[index].style.display = "none";
+      index = index + 1;
+      if (index == 49) {
+        index = 0;
+      }
+      carrusel_img[index].style.display = "flex";
+    }
+  }
+};
+
 $flecha_izquierda.addEventListener("click", nextSliderBtn);
 $flecha_derecha.addEventListener("click", prevSliderBtn);
 
+$flecha_derecha_modal.addEventListener("click", prevSliderBtn_modal);
+$flecha_izquierda_modal.addEventListener("click", nextSliderBtn_modal);
 //maximizar gifo
-const maximizeGif = (gif, username, title) => {
+const maximizeGif = (gif) => {
+  let retrievedObject = JSON.parse(localStorage.getItem("GifosArray"));
   $gitMaximized.classList.remove("hidden");
   $gitMaximized.classList.add("maximizedGif");
-  $gitMaximized.innerHTML = "";
-  const maximizedGifContainer = document.createElement("div");
-  maximizedGifContainer.classList.add("maximizedGif__container");
-  maximizedGifContainer.innerHTML = `
-  <div class="close-btn" id="close-max-btn" onclick="closeMaximized()"></div>
-	<div class="maxGif_Container">
-  <div class="previous-btn-modal arrows">
-  <img src="assets/button-slider-left.svg" 
-  alt="Flecha Previous" id="previous-btn-modal">
-</div>
-		<img class="gifMax" src="${gif}" alt="${title}">
-    <div class="next-btn-modal arrows">
-				<img src="assets/Button-Slider-right.svg" alt="Flecha Next" id="next-btn-modal">
-			</div>
-	</div>
-	<div class="gifMaxActions">
-		<div class="gif__info">
-			<p class="gif_user">${username}</p>
-			<p class="gif_title">${title}</p>
-		</div>
-		<div class="btn-gifMaxActions">
-			<div class="buttonsMax favoriteMax" onclick="addToFav('${gif}', '${username}', '${title}')"></div>
-			<div class="buttonsMax downloadMax" onclick="downloadGif('${gif}','${title}')"></div>
-			</div>
-	</div>`;
-  $gitMaximized.appendChild(maximizedGifContainer);
+  $carousel_modal.innerHTML = "";
+  for (let i = 0; i < retrievedObject[0].data.length; i++) {
+    const div = document.createElement("div");
+    div.classList.add("gif__container_modal");
+    if (gif == retrievedObject[0].data[i].images.original.url) {
+      const inicio = i;
+      div.innerHTML = ` 
+  <img class="gifMax" src="${retrievedObject[0].data[inicio].images.original.url}" alt="${retrievedObject[0].data[i].images.original.title}">
+  <div class="gifMaxActions">
+        <div class="gifMaxActions__btn">
+            <div class="buttonsMax favoriteMax " onclick="addToFav()"></div>
+            <div class="buttonsMax downloadMax" onclick="downloadGif()"></div>
+        </div>
+        <div class="gif-info-modal">
+            <p class="gif_user-modal">data</p>
+            <p class="gif_title-modal">titulo</p>
+        </div>
+    </div>
+  `;
+      $carousel_modal.appendChild(div);
+      div.style.display = "flex";
+    } else {
+      div.innerHTML = ` 
+      <img class="gifMax" src="${retrievedObject[0].data[i].images.original.url}" alt="${retrievedObject[0].data[i].images.original.title}">
+      `;
+      $carousel_modal.appendChild(div);
+      div.style.display = "none";
+    }
+  }
 };
 
 //cerrar modal
